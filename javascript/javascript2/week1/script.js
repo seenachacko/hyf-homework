9004d93626c2f59bcbfa161dccf82d4b660acdac
seenachacko <seenachacko119@gmail.com>
2021-02-22 00:03:49 +0100

    updated js-file script.js added

--- javascript/javascript2/week1/script.js
+++ javascript/javascript2/week1/script.js
@@ -1,22 +1,22 @@
 //find shortest word
 const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
 let shortestWordLength= danishWords[0].length;
-danishWords.forEach(shortestWordinArray)
-function shortestWordinArray(wordsArray){   
-    if(wordsArray.length < shortestWordLength){
-        shortestWordLength=wordsArray.length;
-        console.log('shortest word "'+wordsArray +'" length :'+shortestWordLength);   
+danishWords.forEach(logShortestWord)
+function logShortestWord(wordsInArray){   
+    if(wordsInArray.length < shortestWordLength){
+        shortestWordLength=wordsInArray.length;
+        console.log('shortest word "'+wordsInArray +'" length :'+shortestWordLength);   
     } 
 }
 //Find and count the Danish letters
 const danishString = "Jeg har en blå bil";
-findDanishAlphabets(danishString); // returns {total: 1, å: 1}
+logDanishLetters(danishString); // returns {total: 1, å: 1}
 
 const danishString2 = "Blå grød med røde bær";
-findDanishAlphabets(danishString2); // returns {total: 4, æ: 1, ø: 2, å: 1}
+logDanishLetters(danishString2); // returns {total: 4, æ: 1, ø: 2, å: 1}
 
-function findDanishAlphabets(Sentance){
-    let danishSentance=Sentance.toLowerCase();
+function logDanishLetters(sentance){
+    let danishSentance=sentance.toLowerCase();
     let splitedSentance = danishSentance.split('');
     const letter_æ=splitedSentance.filter(letter=>letter=="æ");
     const letter_ø=splitedSentance.filter(letter=>letter=="ø");
