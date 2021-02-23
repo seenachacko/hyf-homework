0a1c39d3f6956caef5c6d035a7a97b201c5aa3a9
seenachacko <seenachacko119@gmail.com>
2021-02-23 13:33:16 +0100

    modified script.js file

--- javascript/javascript2/week1/script.js
+++ javascript/javascript2/week1/script.js
@@ -9,12 +9,31 @@ function logShortestWord(wordsInArray){
     } 
 }
 //Find and count the Danish letters
+const danishLetters = ['å', 'æ', 'ø'];
+const obj = {};
+function getDanishLetters(str) {
+    let sum = 0;
+    const splittedString = str.split('');
+    for (let i = 0; i < danishLetters.length; i++) {
+        let count = 0;
+        const mappedArray=splittedString.map(function(letters){
+            if (danishLetters[i] === letters) {
+                return count++;
+            }
+        })
+        sum = sum + count;
+        obj.total = sum;
+        obj[danishLetters[i]] = count;
+    }
+    console.log(obj)
+}
 const danishString = "Jeg har en blå bil";
-logDanishLetters(danishString); // returns {total: 1, å: 1}
-
+getDanishLetters(danishString); // returns {total: 1, å: 1}
 const danishString2 = "Blå grød med røde bær";
-logDanishLetters(danishString2); // returns {total: 4, æ: 1, ø: 2, å: 1}
+getDanishLetters(danishString2); // returns {total: 4, æ: 1, ø: 2, å: 1}
 
+
+/*
 function logDanishLetters(sentance){
     let danishSentance=sentance.toLowerCase();
     let splitedSentance = danishSentance.split('');
@@ -24,4 +43,4 @@ function logDanishLetters(sentance){
     const total=letter_æ.length+letter_ø.length+letter_å.length
     const letterObj={total:total,æ:letter_æ.length,ø:letter_ø.length,å:letter_å.length};
     console.log(letterObj);
-}
+}*/
