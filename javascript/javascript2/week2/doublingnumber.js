9e32ece2c559ed4d135efbf7e8ce548a99c1bbf9
seenachacko <seenachacko119@gmail.com>
2021-02-26 19:05:03 +0100

    js file for doubling operation

---
+++ javascript/javascript2/week2/doublingnumber.js
@@ -0,0 +1,4 @@
+let numbers = [1, 2, 3, 4];
+console.log("original Array "+numbers);
+let doubledEvenNumbers = numbers.filter(number => number% 2 !== 0).map(number => number* 2);
+console.log("after doubling operation of odd numbers : "+doubledEvenNumbers);
