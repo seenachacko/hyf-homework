70c73275c9b664f8dcccf9226843a005ca86c6ed
seenachacko <seenachacko119@gmail.com>
2021-04-06 12:17:24 +0200

    feedback implemented

--- databases/week1/week1.sql
+++ databases/week1/week1.sql
@@ -1,48 +1,41 @@
 #1
 SELECT COUNT(*) AS number_of_tasks
 FROM task;
-
 #2
 SELECT COUNT(*)
 FROM task 
 WHERE  due_date IS NULL;
-
 #3
-SELECT * FROM task 
+SELECT task.id,task.title,task.description, status.name 
+FROM task 
  JOIN status on task.status_id=status.id
- WHERE name = "Done";
- 
+ WHERE name = "Done"; 
  #4
- SELECT * FROM task 
+ SELECT task.id,task.title,task.description, status.name
+ FROM task 
  JOIN status on task.status_id=status.id
  WHERE name != "Done";
- 
  #5
- SELECT * 
+ SELECT  task.id,task.title,task.description, task.created, task.updated
  FROM task
  ORDER BY created DESC;
- 
  #6
- SELECT * 
+ SELECT task.id,task.title,task.description, task.created, task.updated
  FROM task
  ORDER BY created DESC LIMIT 1;
- 
  #7
- SELECT title,description
+ SELECT title,due_date
  FROM task
- WHERE title LIKE '%database%' OR description LIKE '%database%';
-
+ WHERE title LIKE "%database%"  OR description LIKE "%datadbase%";
 #8
 SELECT title, status.name AS status
  FROM task 
  JOIN status on task.status_id=status.id;
- 
  #9
 SELECT status.name, COUNT(status.name) AS count
  FROM task 
  JOIN status on task.status_id=status.id
  GROUP BY name;
- 
 #10
 SELECT status.name, COUNT(status.name) 
  FROM task 
