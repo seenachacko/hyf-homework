994b31ffa0554f40aa319469f04dbb5a5d378d71
seenachacko <seenachacko119@gmail.com>
2021-05-14 16:04:43 +0200

    feed back implemented on warmup

--- nodejs/week2/homework/warmup/index.js
+++ nodejs/week2/homework/warmup/index.js
@@ -6,25 +6,37 @@ app.get("/numbers/add", (req, res) => {
     const firstNum = parseInt(req.query.first);
     const secondNum = parseInt(req.query.second);
     try {
-        if(isNaN(firstNum)&&isNaN(secondNum))
-        {
+        if (isNaN(firstNum) && isNaN(secondNum)) {
             res.send(`sum:${firstNum + secondNum}`)
-        }else{
+        } else {
             response.status(400).send(`numbers shoulde interger`);
         }
-        
+
     } catch (error) {
-        response.status(500).send(`Internal Server Error`) ;
-        
+        response.status(500).send(`Internal Server Error`);
+
     }
-    
-    
+
+
 });
 app.get("/numbers/multiply/:first/:second", (req, res) => {
     const firstNum = parseInt(req.params.first);
     const secondNum = parseInt(req.params.second);
-    res.send(`multiply: ${firstNum * secondNum}`)
+    try {
+        if (isNaN(firstNum) && isNaN(secondNum)) {
+            res.send(`sum:${firstNum * secondNum}`)
+        } else {
+            response.status(400).send(`numbers shoulde interger`);
+        }
+
+    } catch (error) {
+        response.status(500).send(`Internal Server Error`);
+
+    }
+
+
 });
+
 
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
