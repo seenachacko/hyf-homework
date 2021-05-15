d7b7ca1b4623457ad3296d8a91a583b1bfe969ba
seenachacko <seenachacko119@gmail.com>
2021-05-15 20:32:03 +0200

    home work added

---
+++ nodejs/week3/homework/warmup/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("nodejs week3 homework"));
+
+app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
+app.get("/calculator/multiply", (req, res) => {
+    const values = Object.values(req.query).flat()
+    console.log(values);
+    const multiply = values.reduce((acc, val) => acc * val)
+    
+    if(isNaN(multiply)){
+       res.status(400).send("values should be integer")
+
+    }else{
+        res.send(`Result : ${multiply}`)
+    }
+
+});
+app.post("/calculator/multiply", (req, res) => {
+    const values = Object.values(req.body).flat()
+    const multipliedResult = values.reduce((acc, val) => acc * val)
+    if(isNaN( multipliedResult) ){
+       res.status(400).send("values should be integer")
+
+    }else{
+        res.send(`Result :${ multipliedResult}`)
+    }
+
+});
+
+app.get("/calculator/subtraction", (req, res) => {
+    const values = Object.values(req.query).flat()
+    const subtraction = values.reduce((acc, val) => acc - val)
+    if(isNaN(subtraction)){
+       res.status(400).send("values should be integer")
+
+    }else{
+        res.send(`Result :${subtraction}`)
+    }
+
+});
+app.get("/calculator/addition", (req, res) => {
+    const values = Object.values(req.query).flat()
+    console.log(values);
+    const addingResult = values.reduce((acc, val) =>(Number(acc) + Number(val)),0)
+
+    if(isNaN(addingResult)){
+       res.status(400).send("values should be integer")
+
+    }else{
+        res.send(`Result :${addingResult}`)
+    }
+
+});
+app.get("/calculator/division", (req, res) => {
+    const values = Object.values(req.query).flat()
+    console.log(values);
+    const dividedResult = values.reduce((acc, val) => acc / val)
+    if(isNaN(dividedResult)){
+       res.status(400).send("values should be integer")
+
+    }else{
+        res.send(`Result :${dividedResult}`)
+    }
+
+});
+
