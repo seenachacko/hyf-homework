d2a8be7ee442f49dcef03c0cbb9e8f87b975dc7a
seenachacko <seenachacko119@gmail.com>
2021-05-20 15:44:39 +0200

    files added

---
+++ react/week1/todo-list-app/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Mylist from './components/Mylist'
+import './App.css';
+function App() {
+  return (
+    <div className="mainContainer">
+      <Header />
+      <Mylist />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/week1/todo-list-app/src/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Todo.css'
+function Todo(props) {
+    console.log(props);
+    return (
+        <div>
+            <table>
+            <tr>
+                <td>{props.item.id}</td>
+                <td>{props.item.todo}</td>
+                <td>{props.item.deadline}</td>
+            </tr>
+            </table>
+        </div>
+    );
+}
+export default Todo;
