aae3f9f27f9449afc0e404268aafa7a2c854a6b9
seenachacko <seenachacko119@gmail.com>
2021-05-21 09:29:12 +0200

    feedback implemented

--- nodejs/week3/homework/warmup/index.js
+++ nodejs/week3/homework/warmup/index.js
@@ -1,18 +1,24 @@
+const { request } = require("express");
 const express = require("express");
 const app = express();
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
 app.get("/calculator/multiply", (req, res) => {
     const values = Object.values(req.query).flat()
-    console.log(values);
     const multiply = values.reduce((acc, val) => acc * val)
-    
-    if(isNaN(multiply)){
-       res.status(400).send("values should be integer")
 
-    }else{
+    if (isNaN(multiply)) {
+        res.status(400).send("values should be integer")
+
+    } else {
         res.send(`Result : ${multiply}`)
     }
 
@@ -20,54 +26,43 @@ app.get("/calculator/multiply", (req, res) => {
 app.post("/calculator/multiply", (req, res) => {
     const values = Object.values(req.body).flat()
     const multipliedResult = values.reduce((acc, val) => acc * val)
-    if(isNaN( multipliedResult) ){
-       res.status(400).send("values should be integer")
+    if (isNaN(multipliedResult)) {
+        res.status(400).send("values should be integer")
 
-    }else{
-        res.send(`Result :${ multipliedResult}`)
+    } else {
+        res.send(`Result :${multipliedResult}`)
     }
 
 });
-/*
-app.post("/multiply", (req, res) =>{
-    let values = Object.values(req.body)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-    let mult = values.reduce((a,b) => a * b)
-    res.send(`${mult}`)
-});*/
 app.get("/calculator/subtraction", (req, res) => {
     const values = Object.values(req.query).flat()
     const subtraction = values.reduce((acc, val) => acc - val)
-    if(isNaN(subtraction)){
-       res.status(400).send("values should be integer")
+    if (isNaN(subtraction)) {
+        res.status(400).send("values should be integer")
 
-    }else{
+    } else {
         res.send(`Result :${subtraction}`)
     }
 
 });
 app.get("/calculator/addition", (req, res) => {
     const values = Object.values(req.query).flat()
-    console.log(values);
-    const addingResult = values.reduce((acc, val) =>(Number(acc) + Number(val)),0)
+    const addingResult = values.reduce((acc, val) => (Number(acc) + Number(val)), 0)
+    if (isNaN(addingResult)) {
+        res.status(400).send("values should be integer")
 
-    if(isNaN(addingResult)){
-       res.status(400).send("values should be integer")
-
-    }else{
+    } else {
         res.send(`Result :${addingResult}`)
     }
 
 });
 app.get("/calculator/division", (req, res) => {
     const values = Object.values(req.query).flat()
-    console.log(values);
     const dividedResult = values.reduce((acc, val) => acc / val)
-    if(isNaN(dividedResult)){
-       res.status(400).send("values should be integer")
+    if (isNaN(dividedResult)) {
+        res.status(400).send("values should be integer")
 
-    }else{
+    } else {
         res.send(`Result :${dividedResult}`)
     }
 
