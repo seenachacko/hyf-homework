20ab70a9c803ab417b6b431e08907870e03a2b2b
seenachacko <seenachacko119@gmail.com>
2021-06-11 22:04:17 +0200

    implementing feedbacks

--- react/week3/todo-app/src/Components/Header.js
+++ react/week3/todo-app/src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Header = () => {
   return (
-    <div className="header-Container">
+    <div className="header-container">
       <h3>My TODO List</h3>
     </div>
   );
